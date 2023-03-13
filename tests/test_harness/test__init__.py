a49716dbdb0111a346062660521a116c3e579d6c
Freddie Mather <freddie.mather@smartdcs.co.uk>
2023-03-13 11:04:18 +0000

    CDSDT-72 Test no file name; check response data

--- tests/test_harness/test__init__.py
+++ tests/test_harness/test__init__.py
@@ -1,6 +1,6 @@
 """Testing __init__.py
 """
-
+import os
 from pathlib import Path
 
 from flask.testing import FlaskClient
@@ -19,4 +19,27 @@ def test_bad_mime_type(client: FlaskClient) -> None:
         "/uploadUML",
         content_type="application/json",
     )
+    assert response.data == b"mime-type must be multipart/form-data\n"
+    assert response.status_code == 400
+
+
+def test_no_file_name(client: FlaskClient) -> None:
+    """Test no file name given
+
+    :param client: Flask test client
+    :type client: :class:`FlaskClient`
+    """
+    proper_file_path = os.path.join(resources, "test_uml_1.puml")
+    file_data = open(proper_file_path, "rb")
+    data = {
+        "file": (file_data, "")
+    }
+
+    response = client.post(
+        "/uploadUML",
+        data=data,
+        buffered=True,
+        content_type="multipart/form-data"
+    )
+    assert response.data == b"One of the uploaded files has no filename\n"
     assert response.status_code == 400
