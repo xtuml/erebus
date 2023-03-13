50aa2c561472c5d989a5846e4084fd235c1f50dc
Freddie Mather <freddie.mather@smartdcs.co.uk>
2023-03-13 10:27:00 +0000

    CDSDT-72 Created bad mime type test

--- tests/test_harness/test__init__.py
+++ tests/test_harness/test__init__.py
@@ -1,2 +1,22 @@
 """Testing __init__.py
 """
+
+from pathlib import Path
+
+from flask.testing import FlaskClient
+
+# get resources folder in tests folder
+resources = Path(__file__).parent / "test_files"
+
+
+def test_bad_mime_type(client: FlaskClient) -> None:
+    """Test bad mime-type given
+
+    :param client: Flask test client
+    :type client: :class:`FlaskClient`
+    """
+    response = client.post(
+        "/uploadUML",
+        content_type="application/json",
+    )
+    assert response.status_code == 400
