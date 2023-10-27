3f6987bee307fe8c7ee5db7b494987a75d437e58
Nathanael Farley <nathanael.farley@smartdcs.co.uk>
2023-10-27 14:28:14 +0000

    Added timeout to retry curl commands until test harness is ready.

---
+++ end-to-end-curl-commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# We do a while loop here to keep requesting the uploadUML endpoint until the test harness is up and running
+while ! curl --location --request POST 'http://127.0.0.1:8800/uploadUML' --form 'file1=@"./tests/test_harness/test_files/test_uml_1.puml"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
+    echo tried to do a curl request to uploadUML endpoint but it failed, trying again in 1 second
+    echo printing test harness logs to see what is going on
+    docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail 
+    sleep 1
+done
+curl -X POST -d '{"TestName": "demo4", "TestConfig":{"event_gen_options":{"invalid":false}, "type":"Performance", "performance_options": {"num_files_per_sec":10, "total_jobs":100, "shard":true}}}' -H 'Content-Type: application/json' 'http://127.0.0.1:8800/startTest'
