f7980d6be0bee94c17897e92919bdf1a5fd3c658
Nathanael Farley <nathanael.farley@smartdcs.co.uk>
2023-10-30 11:19:26 +0000

    Edited to have less load.

--- end-to-end-curl-commands.sh
+++ end-to-end-curl-commands.sh
@@ -9,4 +9,4 @@ while ! curl --location --request POST 'http://127.0.0.1:8800/uploadUML' --form 
     docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail 
     sleep 1
 done
-curl -X POST -d '{"TestName": "demo4", "TestConfig":{"event_gen_options":{"invalid":false}, "type":"Performance", "performance_options": {"num_files_per_sec":500, "total_jobs":5000, "shard":true}}}' -H 'Content-Type: application/json' 'http://127.0.0.1:8800/startTest'
+curl -X POST -d '{"TestName": "demo4", "TestConfig":{"event_gen_options":{"invalid":false}, "type":"Performance", "performance_options": {"num_files_per_sec":50, "total_jobs":500, "shard":true}}}' -H 'Content-Type: application/json' 'http://127.0.0.1:8800/startTest'
