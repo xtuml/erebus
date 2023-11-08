a4ed8445d1dbd7ec38f3a2b7afc52275294c01c5
JackHintonSmartDCSIT <jack.hinton@smartdcs.co.uk>
2023-11-08 16:19:28 +0000

    [CDSTH-337] Removed --location flag from curl in end-to-end test

--- end-to-end-curl-commands.sh
+++ end-to-end-curl-commands.sh
@@ -3,7 +3,7 @@
 set -e
 
 # We do a while loop here to keep requesting the uploadUML endpoint until the test harness is up and running
-while ! curl --location --request POST 'http://127.0.0.1:8800/uploadUML' --form 'file1=@"./tests/test_harness/test_files/test_uml_1.puml"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
+while ! curl --request POST 'http://127.0.0.1:8800/uploadUML' --form 'file1=@"./tests/test_harness/test_files/test_uml_1.puml"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
     echo tried to do a curl request to uploadUML endpoint but it failed, trying again in 1 second
     echo printing test harness logs to see what is going on
     docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail 
