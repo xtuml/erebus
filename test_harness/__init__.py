690a4791e11b07e3029b282886451b2167504181
JackHintonSmartDCSIT <jack.hinton@smartdcs.co.uk>
2023-11-09 10:54:17 +0000

    [CDSTH-337] Trying a different way of setting content-length

--- test_harness/__init__.py
+++ test_harness/__init__.py
@@ -179,7 +179,7 @@ class HarnessApp(Flask):
                         },
                     }
                 ), 200
-            returnVal.content_length += 334
+            returnVal.headers["Content-Length"] += 334
             return returnVal
 
         return jsonify({"running": False}), 200
