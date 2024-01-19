b5faf017b7ac2b2f8c4de05a2855f952bb63eeb2
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-01-19 12:59:55 +0000

    CDSTH-492 Created MessageSender class and refactored inputs to methods

--- test_harness/message_buses/message_buses.py
+++ test_harness/message_buses/message_buses.py
@@ -11,24 +11,32 @@ import aiohttp
 from test_harness.utils import wrap_kafka_future
 
 
-class MessageConverter(ABC):
+class InputConverter(ABC):
     """Abstract class for message converter.
     """
 
     @abstractmethod
-    def convert_message(self, message_data: Any) -> Any:
+    def convert_input(
+        self,
+        message: Any,
+        *args,
+        **kwargs
+    ) -> tuple[Any, tuple, dict, tuple, dict]:
         """Abstract method to convert a message.
         """
         pass
 
 
-class SimpleMessageConverter(MessageConverter):
+class SimpleInputConverter(InputConverter):
     """Simple message converter.
     """
-    def convert_message(self, message_data: Any) -> Any:
+    def convert_input(
+        self,
+        message: Any
+    ) -> tuple[Any, tuple, dict, tuple, dict]:
         """Convert a message.
         """
-        return message_data
+        return message, (), {}, (), {}
 
 
 class ResponseConverter(ABC):
@@ -36,7 +44,7 @@ class ResponseConverter(ABC):
     """
 
     @abstractmethod
-    def convert_response(self, response_data: Any) -> Any:
+    def convert_response(self, response: Any, *args, **kwargs) -> Any:
         """Abstract method to convert a response.
         """
         pass
@@ -45,10 +53,10 @@ class ResponseConverter(ABC):
 class SimpleResponseConverter(ResponseConverter):
     """Simple response converter.
     """
-    def convert_response(self, response_data: Any) -> Any:
+    def convert_response(self, response: Any) -> Any:
         """Convert a response.
         """
-        return response_data
+        return response
 
 
 class MessageProducer(ABC):
@@ -57,36 +65,19 @@ class MessageProducer(ABC):
     def __init__(
         self,
         message_bus: "MessageBusSendingController",
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None
     ) -> None:
         """Constructor method
         """
         self.message_bus = message_bus
-        self._message_converter = (
-            message_converter
-            if message_converter is not None
-            else SimpleMessageConverter()
-        )
-        self._response_converter = (
-            response_converter
-            if response_converter is not None
-            else SimpleResponseConverter()
-        )
 
-    async def send_message(self, message: Any) -> None:
+    async def send_message(self, message: Any, *args, **kwargs) -> None:
         """Abstract method to send a message.
         """
-        converted_message = self._message_converter.convert_message(message)
-        raw_response = await self._send(converted_message)
-        converted_response = self._response_converter.convert_response(
-            raw_response
-        )
-        return converted_response
+        return await self._send(message, *args, **kwargs)
 
     @abstractmethod
-    async def _send(self, converted_message: Any) -> Any:
-        """Abstract method to send a converted message.
+    async def _send(self, message: Any, *args, **kwargs) -> Any:
+        """Abstract method to send a message.
         """
         pass
 
@@ -116,8 +107,6 @@ class MessageBusSendingController(ABC):
     def get_message_producer(
         self,
         *args,
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None,
         **kwargs,
     ) -> "MessageProducer":
         """Abstract method to get a message producer.
@@ -125,7 +114,7 @@ class MessageBusSendingController(ABC):
         pass
 
     @abstractmethod
-    async def send(self, data: Any, *args, **kwargs) -> None:
+    async def send(self, message: Any, *args, **kwargs) -> None:
         """Abstract method to send a message.
         """
         pass
@@ -138,23 +127,19 @@ class KafkaMessageProducer(MessageProducer):
         self,
         topic: str,
         message_bus: "KafkaMessageBus",
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None,
     ) -> None:
         """Constructor method
         """
         self.topic = topic
         super().__init__(
             message_bus=message_bus,
-            message_converter=message_converter,
-            response_converter=response_converter,
         )
 
-    async def _send(self, converted_message: bytes) -> bytes:
+    async def _send(self, message: bytes) -> bytes:
         """Send a converted message.
         """
         return await self.message_bus.send(
-            data=converted_message,
+            message=message,
             topic=self.topic,
         )
 
@@ -188,23 +173,18 @@ class KafkaMessageBus(MessageBusSendingController):
     def get_message_producer(
         self,
         topic: str,
-        *,
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None,
     ) -> "MessageProducer":
         """Get a message producer.
         """
         return KafkaMessageProducer(
             topic=topic,
             message_bus=self,
-            message_converter=message_converter,
-            response_converter=response_converter,
         )
 
-    async def send(self, data: bytes, topic: str) -> bytes:
+    async def send(self, message: bytes, topic: str) -> bytes:
         """Send a message to a topic.
         """
-        return await self._send_to_topic(data=data, topic=topic)
+        return await self._send_to_topic(data=message, topic=topic)
 
     @abstractmethod
     async def _send_to_topic(self, data: bytes, topic: str) -> bytes:
@@ -254,11 +234,12 @@ class AIOKafkaMessageBus(KafkaMessageBus):
         """
         await asyncio.wait_for(self.producer.stop())
 
-    async def _send_to_topic(self, data: bytes, topic: str) -> bytes:
+    async def _send_to_topic(self, message: bytes, topic: str) -> bytes:
         """Send a message to a topic.
         """
-        await self.producer.send_and_wait(topic=topic, value=data)
-        return data
+        return await self.producer.send_and_wait(
+            topic=topic, value=message
+        )
 
 
 class Kafka3MessageBus(KafkaMessageBus):
@@ -317,24 +298,21 @@ class HTTPMessageProducer(MessageProducer):
         self,
         url: str,
         message_bus: "HTTPMessageBus",
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None,
     ) -> None:
         """Constructor method
         """
         self.url = url
         super().__init__(
             message_bus=message_bus,
-            message_converter=message_converter,
-            response_converter=response_converter,
         )
 
-    async def _send(self, converted_message: bytes) -> bytes:
+    async def _send(self, message: Any, **kwargs) -> Any:
         """Send a converted message.
         """
         return await self.message_bus.send(
-            data=converted_message,
+            message=message,
             url=self.url,
+            **kwargs
         )
 
 
@@ -370,28 +348,27 @@ class HTTPMessageBus(MessageBusSendingController):
     def get_message_producer(
         self,
         url: str,
-        *,
-        message_converter: MessageConverter | None = None,
-        response_converter: ResponseConverter | None = None,
     ) -> "MessageProducer":
         """Get a message producer.
         """
         return HTTPMessageProducer(
             url=url,
             message_bus=self,
-            message_converter=message_converter,
-            response_converter=response_converter,
         )
 
-    async def send(self, data: bytes, url: str) -> bytes:
+    async def send(self, message: Any, url: str, **kwargs) -> bytes:
         """Send a message to a url.
         """
-        return await self._send_post_to_url(data=data, url=url)
+        return await self._send_post_to_url(message, url, **kwargs)
 
-    async def _send_post_to_url(self, data: bytes, url: str) -> bytes:
+    async def _send_post_to_url(
+        self, message: Any, url: str, **kwargs
+    ) -> bytes:
         """Abstract method to send a message to a url.
         """
-        async with self.session.post(url=url, data=data) as response:
+        async with self.session.post(
+            url=url, data=message, **kwargs
+        ) as response:
             return await response.read()
 
     async def __aenter__(self) -> Self:
@@ -404,3 +381,44 @@ class HTTPMessageBus(MessageBusSendingController):
         """Exit the context.
         """
         await self.session.__aexit__(exc_type, exc, tb)
+
+
+class MessageSender(ABC):
+    def __init__(
+        self,
+        producer: MessageProducer,
+        input_converter: InputConverter | None = None,
+        response_converter: ResponseConverter | None = None
+    ) -> None:
+        self._producer = producer
+        self._input_converter = (
+            input_converter
+            if input_converter is not None
+            else SimpleInputConverter()
+        )
+        self._response_converter = (
+            response_converter
+            if response_converter is not None
+            else SimpleResponseConverter()
+        )
+
+    async def send(self, message: Any, *args, **kwargs) -> Any:
+        (
+            convert_data,
+            convert_args,
+            convert_kwargs,
+            response_args,
+            response_kwargs
+        ) = self._input_converter.convert_input(message, *args, **kwargs)
+        response = await self._sender(
+            convert_data, *convert_args, **convert_kwargs
+        )
+        return self._response_converter.convert_response(
+            response=response,
+            *response_args,
+            **response_kwargs
+        )
+
+    @abstractmethod
+    async def _sender(self, converted_data: Any, *args, **kwargs) -> Any:
+        pass
