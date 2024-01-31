406444a9eb7a531e8ed768ca3281c3d92f05dffd
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-01-31 09:13:45 +0000

    CDSTH-492 Test for get producer context

--- tests/test_harness/message_buses/test_message_buses.py
+++ tests/test_harness/message_buses/test_message_buses.py
@@ -4,7 +4,8 @@ import pytest
 import aioresponses
 
 from test_harness.message_buses.message_buses import (
-    Kafka3MessageBus, AIOKafkaMessageBus, HTTPMessageBus
+    Kafka3MessageBus, AIOKafkaMessageBus, HTTPMessageBus,
+    get_producer_context
 )
 
 
@@ -172,6 +173,91 @@ class TestMessageProducer:
                     url="http://localhost:8080/topics/test_topic"
                 )
                 result = await http_message_producer.send_message(
+                    message=b"message"
+                )
+        text = await result.text()
+        assert text == "test response"
+
+
+class TestGetProducerContext:
+    """Tests for `get_producer_context`
+    """
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_get_producer_context_aio_kafka(
+        aio_kafka_producer_mock: list[str]
+    ) -> None:
+        """Tests `get_producer_context` for kafka message bus
+
+        :param aio_kafka_producer_mock: Mock kafka producer
+        :type aio_kafka_producer_mock: `list`[`str`]
+        """
+        async with get_producer_context(
+            message_bus="KAFKA",
+            message_bus_kwargs=dict(
+                bootstrap_servers="localhost:9092"
+            ),
+            producer_kwargs=dict(
+                topic="test_topic"
+            )
+        ) as producer:
+            result = await producer.send_message(
+                message=b"message"
+            )
+        assert result == ""
+        assert aio_kafka_producer_mock[0] == "start"
+        assert aio_kafka_producer_mock[1] == "send"
+        assert aio_kafka_producer_mock[2] == "stop"
+
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_get_producer_context_kafka3(
+        sync_kafka_producer_mock: list[str]
+    ) -> None:
+        """Tests `get_producer_context` for kafka message bus
+
+        :param sync_kafka_producer_mock: Mock kafka producer
+        :type sync_kafka_producer_mock: `list`[`str`]
+        """
+        async with get_producer_context(
+            message_bus="KAFKA3",
+            message_bus_kwargs=dict(
+                bootstrap_servers="localhost:9092"
+            ),
+            producer_kwargs=dict(
+                topic="test_topic"
+            )
+        ) as producer:
+            result = await producer.send_message(
+                message=b"message"
+            )
+        assert result == ""
+        assert sync_kafka_producer_mock[0] == "start"
+        assert sync_kafka_producer_mock[1] == "send"
+        assert sync_kafka_producer_mock[2] == "stop"
+
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_get_producer_context_http() -> None:
+        """Tests `get_producer_context` for http message bus
+
+        :param http_client_mock: Mock http client
+        :type http_client_mock: `list`[`str`]
+        """
+        with aioresponses.aioresponses() as mock:
+            mock.post(
+                "http://localhost:8080/topics/test_topic",
+                status=200,
+                body="test response"
+            )
+            async with get_producer_context(
+                message_bus="HTTP",
+                message_bus_kwargs=dict(),
+                producer_kwargs=dict(
+                    url="http://localhost:8080/topics/test_topic"
+                )
+            ) as producer:
+                result = await producer.send_message(
                     message=b"message"
                 )
         text = await result.text()
