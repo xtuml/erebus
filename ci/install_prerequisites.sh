705438cd12ab53cabd4902336c4baebbdb2dc414
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-05-16 12:06:14 +0100

    Wipe files before internal repo merge

--- ci/install_prerequisites.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /tmp/
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/smartdcs1/cdsdt/test-event-generator.git
-cd test-event-generator
-git checkout CDSDT-41-test-event-generator
-pip install -r requirements.txt
-pip install .
-cd ..
-rm -f -r test-event-generator/
