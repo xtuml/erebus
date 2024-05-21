9bc9af699d361e803dd2f48520e584f4365633b4
Zac Harrison <zac.harrison@smartdcsit.co.uk>
2024-05-21 15:39:12 +0100

    migrate mock_pv_http_interface to new tests folder wtihin pv package

--- test_harness/utils.py
+++ test_harness/utils.py
@@ -10,11 +10,8 @@ import asyncio
 import shutil
 from threading import Thread
 from multiprocessing import Queue
-from contextlib import contextmanager
 import json
 
-import responses
-from aioresponses import aioresponses, CallbackResult
 import flatdict
 from tqdm import tqdm
 import numpy as np
@@ -499,63 +496,3 @@ class PVLogFileNameCallback:
             )
         else:
             return 404, {}, json.dumps({})
-
-
-@contextmanager
-def mock_pv_http_interface(
-    harness_config: HarnessConfig,
-    send_pv_callback: Callable[..., CallbackResult] | None = None,
-    log_return_callback: Callable[..., tuple[int, dict, str]] | None = None,
-    log_file_name_call_back: Callable[..., tuple[int, dict, str]] | None = None
-) -> Generator[aioresponses, Any, None]:
-    """Context manager to mock the pv http interface
-
-    :param harness_config: The harness config
-    :type harness_config: :class:`HarnessConfig`
-    :param send_pv_callback: The callback for the send pv function, defaults to
-    `None`
-    :type send_pv_callback: :class:`Callable`[..., :class:`CallbackResult`],
-    optional
-    :param log_return_callback: The callback for the log return function,
-    defaults to `None`
-    :type log_return_callback: :class:`Callable`[..., `tuple`[`int`, `dict`,
-    `str`]], optional
-    :param log_file_name_call_back: The callback for the log file name
-    function, defaults to `None`
-    :type log_file_name_call_back: :class:`Callable`[..., `tuple`[`int`, `dict`
-    , `str`]], optional
-    :return: Yields the mock
-    :rtype: :class:`Generator`[:class:`aioresponses`, `Any`, `None`]
-    """
-    if log_file_name_call_back is None:
-        log_file_name_call_back = PVLogFileNameCallback(
-            harness_config
-        ).call_back
-    responses.add_callback(
-        responses.POST,
-        harness_config.log_urls["location"]["getFileNames"],
-        callback=log_file_name_call_back,
-    )
-    if log_return_callback is None:
-        responses.post(
-            url=harness_config.log_urls["location"]["getFile"],
-            body=b"test log",
-        )
-    else:
-        responses.add_callback(
-            responses.POST,
-            harness_config.log_urls["location"]["getFile"],
-            callback=log_return_callback,
-        )
-    responses.get(
-            url=harness_config.pv_clean_folders_url
-        )
-    responses.post(
-        url=harness_config.pv_send_job_defs_url
-    )
-    with aioresponses() as mock:
-        mock.post(
-            url=harness_config.pv_send_url, repeat=True,
-            callback=send_pv_callback
-        )
-        yield mock
