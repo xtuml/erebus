5424c34da94669b4078a4b2d3716526ff0d8e769
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-05-22 12:24:31 +0000

    update test to reference PV Config

--- tests/test_harness/process_manager/test_process_manager__init__.py
+++ tests/test_harness/process_manager/test_process_manager__init__.py
@@ -1,6 +1,5 @@
 # pylint: disable=R0801
-"""Tests for __init__.py
-"""
+"""Tests for __init__.py"""
 
 from pathlib import Path
 import os
@@ -10,36 +9,31 @@ import shutil
 
 import responses
 
-from test_harness.config.config import TestConfig, HarnessConfig
-from test_harness.process_manager import (
-    harness_test_manager
-)
+from test_harness.config.config import TestConfig
+from test_harness.protocol_verifier.config.config import ProtocolVerifierConfig
+from test_harness.process_manager import harness_test_manager
 from test_harness.utils import clean_directories
-from test_harness.protocol_verifier.mocks.mock_pv_http_interface \
-      import mock_pv_http_interface
+from test_harness.protocol_verifier.mocks.mock_pv_http_interface import (
+    mock_pv_http_interface,
+)
 
 # get test config
 test_config_path = os.path.join(
-    Path(__file__).parent.parent,
-    "config/test_config.config"
+    Path(__file__).parent.parent, "config/test_config.config"
 )
 
 # get path of tests uml file
 test_file_path = os.path.join(
-    Path(__file__).parent.parent / "test_files",
-    "test_uml_job_def.puml"
+    Path(__file__).parent.parent / "test_files", "test_uml_job_def.puml"
 )
 
-uuid4hex = re.compile(
-            '[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\\Z', re.I
-        )
+uuid4hex = re.compile("[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\\Z", re.I)
 
 
 @responses.activate
 def test_harness_test_manager_uml_exists() -> None:
-    """Tests `harness_test_manager` when uml exists in the uml file store
-    """
-    harness_config = HarnessConfig(test_config_path)
+    """Tests `harness_test_manager` when uml exists in the uml file store"""
+    harness_config = ProtocolVerifierConfig(test_config_path)
     test_config = TestConfig()
     shutil.copy(test_file_path, harness_config.uml_file_store)
     with mock_pv_http_interface(harness_config):
@@ -51,28 +45,30 @@ def test_harness_test_manager_uml_exists() -> None:
     assert success
     files = glob.glob("*.*", root_dir=harness_config.report_file_store)
     expected_files = [
-        "Results.csv", "Results.html", "Results.xml",
+        "Results.csv",
+        "Results.html",
+        "Results.xml",
         "Results_Aggregated.html",
-        "Verifier.log", "Reception.log",
+        "Verifier.log",
+        "Reception.log",
     ]
     for file in files:
         file_in_files = file in expected_files
-        is_uuid = bool(uuid4hex.match(
-                file.replace("-", "").replace(".json", "")
-            ))
+        is_uuid = bool(
+            uuid4hex.match(file.replace("-", "").replace(".json", ""))
+        )
         assert any([file_in_files, is_uuid])
 
-    clean_directories([
-        harness_config.report_file_store,
-        harness_config.uml_file_store
-    ])
+    clean_directories(
+        [harness_config.report_file_store, harness_config.uml_file_store]
+    )
 
 
 def test_harness_test_manager_no_uml() -> None:
     """Tests `harness_test_manager` when uml does not exist in the uml file
     store
     """
-    harness_config = HarnessConfig(test_config_path)
+    harness_config = ProtocolVerifierConfig(test_config_path)
     test_config = TestConfig()
     success, message = harness_test_manager(
         harness_config=harness_config,
