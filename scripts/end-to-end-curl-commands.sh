f096324b0e932741c7d78095f81fb7f84e001060
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-06-18 13:52:31 +0100

    update tests to make them complete faster

--- scripts/end-to-end-curl-commands.sh
+++ scripts/end-to-end-curl-commands.sh
@@ -3,7 +3,7 @@
 set -e
 
 # We do a while loop here to keep requesting the uploadUML endpoint until the test harness is up and running
-echo "Checking if test harness is up and running by uploading"
+echo "Checking if test harness is up and running by uploading test files"
 while ! curl --location --request POST 'http://127.0.0.1:8800/upload/named-zip-files' --form 'performance_test=@"tests/test_harness/test_files/test_zip_file.zip"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
     echo tried to do a curl request to named-zip-files endpoint but it failed, trying again in 1 second
     echo printing test harness logs to see what is going on
