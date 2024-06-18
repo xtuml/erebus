dd22ee58e373fbbf6470cebdc5309eab6a9b2973
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-06-18 10:42:41 +0100

    remove timeout in e2e test

--- scripts/end-to-end-curl-commands.sh
+++ scripts/end-to-end-curl-commands.sh
@@ -5,9 +5,9 @@ set -e
 # We do a while loop here to keep requesting the uploadUML endpoint until the test harness is up and running
 echo "Checking if test harness is up and running by uploading"
 while ! curl --location --request POST 'http://127.0.0.1:8800/upload/named-zip-files' --form 'performance_test=@"tests/test_harness/test_files/test_zip_file.zip"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
-    echo tried to do a curl request to uploadUML endpoint but it failed, trying again in 1 second
+    echo tried to do a curl request to named-zip-files endpoint but it failed, trying again in 1 second
     echo printing test harness logs to see what is going on
-    docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail 
+    docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail
     sleep 1
 done
 echo "Uploading test config"
