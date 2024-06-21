8546eba58a6cdb9b3f31b87cc2e90bae9eff40a5
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-06-21 10:56:36 +0100

    add bash script to pull and run latest tagged version protocol veridier

---
+++ scripts/install_and_run_protocol_verifier.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# This script pulls the latest tagged version of protocol verifier,
+# updates necessary files to run protocol verifier using docker compose and receive
+# passing tests
+
+# NOTE: This script should be run within the root directory of erebus
+# ./scripts/install_and_run_protocol_verifier.sh
+
+# Change working directory to root and pull munin repo
+git clone https://github.com/xtuml/munin.git
+cd munin
+
+# returns latest tag sorted by date in the form refs/tags/<tag_name>
+latest_pv_tag_ref=$(git for-each-ref --sort=creatordate --format '%(refname)' refs/tags | tail -1)
+
+# format latest tag to just <tag_name>
+latest_pv_tag=${latest_pv_tag_ref:10}
+
+echo "Checking out latest tag: $latest_pv_tag"
+git checkout tags/$latest_pv_tag
+
+# Copy specific files over to munin repo to get tests to pass
+echo "Copying over required property files to munin"
+cd ..
+cp ./end_to_end_test_files/log-pv-files.properties ./munin/deploy/config/log-pv-files.properties
+cp ./end_to_end_test_files/log-pv-kafka.properties ./munin/deploy/config/log-pv-kafka.properties
+
+echo "Replacing docker compose file within munin /deploy"
+cp ./end_to_end_test_files/docker-compose.prod.yml ./munin/deploy/docker-compose.yml
+
+# Update IP address for KAFKA_ADVERTISED_LISTENERS within copied docker-compose
+sed -i 's/172.17.0.1/host.docker.internal/g' ./munin/deploy/docker-compose.yml
+
+echo "Starting up the protocol verifier"
+docker compose -f ./munin/deploy/docker-compose.yml up -d
+
+echo "Starting up the test harness"
+docker compose -f ./docker-compose.yml up -d
