a36fd05f3059df63e252f8327d11a5c8c39ba523
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-09-06 10:47:48 +0000

    87 - Update end to end tests to test using jobdef json

---
+++ scripts/end-to-end-curl-commands-functional-test-jobdefjson.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# We do a while loop here to keep requesting the named-zip-files endpoint until the test harness is up and running
+echo "Checking if test harness is up and running by uploading test files"
+while ! curl --location --request POST 'http://127.0.0.1:8800/upload/named-zip-files' --form 'functional_test_jobdef=@"tests/test_harness/test_files/test_jobdefjson.zip"' -s -o /dev/null -w "%{http_code}" | grep -q 200; do
+    echo tried to do a curl request to named-zip-files endpoint but it failed, trying again in 1 second
+    echo printing test harness logs to see what is going on
+    docker compose -f ./docker-compose-end-to-end-test.yml logs test-harness | tail
+    sleep 1
+done
+echo "Uploading test config"
+curl -X POST -d '{"TestName": "functional_test_jobdef", "TestConfig":{"type":"Functional"}}' -H 'Content-Type: application/json' 'http://127.0.0.1:8800/startTest'
+echo "Waiting for functional test to start"
+sleep 1
